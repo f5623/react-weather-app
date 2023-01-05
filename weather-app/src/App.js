16f9c7ba27c6b1f73fa8ff70ad0a4f03338fee57
fafa <112162769+f5623@users.noreply.github.com>
2023-01-05 12:36:52 +0330

    edit link

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <small className="footnote">
       <a href="https://github.com/f5623/react-weather-app">This project </a>
       is coded by <a href="https://github.com/f5623">Fafa</a> as
-      <a href="https://www.shecodes.io/">SheCodes React </a> final project.
+      <a href="https://www.shecodes.io/">SheCodes React </a>  project.
      </small>
     </div>
   );
